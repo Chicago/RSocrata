ddb8fbb32325d55cee0af0301b8cacbfc9604880
Tom Schenk Jr <tomschenkjr@gmail.com>
2015-07-10 00:45:40 -0500

    All tests now working appropriately

--- tests/testthat/test-all.R
+++ tests/testthat/test-all.R
@@ -9,39 +9,39 @@ context("posixify function")
 
 test_that("posixify returns Long format", {
   dt <- posixify("09/14/2012 10:38:01 PM")
-  expect_equal("POSIXlt", class(dt)[1], "first data type of a date")
-  expect_equal(2012, dt$year + 1900, "year")
-  expect_equal(9, dt$mon + 1, "month")
-  expect_equal(14, dt$mday, "day")
-  expect_equal(22, dt$hour, "hours")
-  expect_equal(38, dt$min, "minutes")
-  expect_equal(1, dt$sec, "seconds")
+  expect_equal("POSIXlt", class(dt)[1], label="first data type of a date")
+  expect_equal(2012, dt$year + 1900, label="year")
+  expect_equal(9, dt$mon + 1, label="month")
+  expect_equal(14, dt$mday, label="day")
+  expect_equal(22, dt$hour, label="hours")
+  expect_equal(38, dt$min, label="minutes")
+  expect_equal(1, dt$sec, label="seconds")
 })
 
 
 test_that("posixify returns Short format", {
   dt <- posixify("09/14/2012")
-  expect_equal("POSIXlt", class(dt)[1], "first data type of a date")
-  expect_equal(2012, dt$year + 1900, "year")
-  expect_equal(9, dt$mon + 1, "month")
-  expect_equal(14, dt$mday, "day")
-  expect_equal(0, dt$hour, "hours")
-  expect_equal(0, dt$min, "minutes")
-  expect_equal(0, dt$sec, "seconds")
+  expect_equal("POSIXlt", class(dt)[1], label="first data type of a date")
+  expect_equal(2012, dt$year + 1900, label="year")
+  expect_equal(9, dt$mon + 1, label="month")
+  expect_equal(14, dt$mday, label="day")
+  expect_equal(0, dt$hour, label="hours")
+  expect_equal(0, dt$min, label="minutes")
+  expect_equal(0, dt$sec, label="seconds")
 })
 
 context("read Socrata")
 
 test_that("read Socrata CSV", {
   df <- read.socrata('https://soda.demo.socrata.com/resource/4334-bgaj.csv')
-  expect_equal(1007, nrow(df), "rows")
-  expect_equal(9, ncol(df), "columns")
+  expect_equal(1007, nrow(df), label="rows")
+  expect_equal(9, ncol(df), label="columns")
 })
 
 test_that("read Socrata JSON", {
   df <- read.socrata('https://soda.demo.socrata.com/resource/4334-bgaj.json')
-  expect_equal(1007, nrow(df), "rows")
-  expect_equal(11, ncol(df), "columns")
+  expect_equal(1007, nrow(df), label="rows")
+  expect_equal(11, ncol(df), label="columns")
 })
 
 test_that("read Socrata No Scheme", {
@@ -50,8 +50,8 @@ test_that("read Socrata No Scheme", {
 
 test_that("readSoQL", {
   df <- read.socrata('http://soda.demo.socrata.com/resource/4334-bgaj.csv?$select=region')
-  expect_equal(1007, nrow(df), "rows")
-  expect_equal(1, ncol(df), "columns")
+  expect_equal(1007, nrow(df), label="rows")
+  expect_equal(1, ncol(df), label="columns")
 })
 
 test_that("readSoQLColumnNotFound (will fail)", {
@@ -65,8 +65,8 @@ test_that("URL is private (Unauthorized) (will fail)", {
 
 test_that("readSocrataHumanReadable", {
   df <- read.socrata('https://soda.demo.socrata.com/dataset/USGS-Earthquake-Reports/4334-bgaj')
-  expect_equal(1007, nrow(df), "rows")
-  expect_equal(9, ncol(df), "columns")
+  expect_equal(1007, nrow(df), label="rows")
+  expect_equal(9, ncol(df), label="columns")
 })
 
 test_that("format is not supported", {
@@ -79,76 +79,76 @@ context("Socrata Calendar")
 test_that("Calendar Date Long", {
   df <- read.socrata('http://soda.demo.socrata.com/resource/4334-bgaj.csv')
   dt <- df$Datetime[1] # "2012-09-14 22:38:01"
-  expect_equal("POSIXlt", class(dt)[1], "data type of a date")
-  expect_equal(2012, dt$year + 1900, "year")
-  expect_equal(9, dt$mon + 1, "month")
-  expect_equal(14, dt$mday, "day")
-  expect_equal(22, dt$hour, "hours")
-  expect_equal(38, dt$min, "minutes")
-  expect_equal(1, dt$sec, "seconds")
+  expect_equal("POSIXlt", class(dt)[1], label="data type of a date")
+  expect_equal(2012, dt$year + 1900, label="year")
+  expect_equal(9, dt$mon + 1, label="month")
+  expect_equal(14, dt$mday, label="day")
+  expect_equal(22, dt$hour, label="hours")
+  expect_equal(38, dt$min, label="minutes")
+  expect_equal(1, dt$sec, label="seconds")
 })
 
 test_that("Calendar Date Short", {
   df <- read.socrata('http://data.cityofchicago.org/resource/y93d-d9e3.csv?$order=debarment_date')
   dt <- df$DEBARMENT.DATE[1] # "05/21/1981"
-  expect_equal("POSIXlt", class(dt)[1], "data type of a date")
-  expect_equal(81, dt$year, "year")
-  expect_equal(5, dt$mon + 1, "month")
-  expect_equal(21, dt$mday, "day")
-  expect_equal(0, dt$hour, "hours")
-  expect_equal(0, dt$min, "minutes")
-  expect_equal(0, dt$sec, "seconds")
+  expect_equal("POSIXlt", class(dt)[1], label="data type of a date")
+  expect_equal(81, dt$year, label="year")
+  expect_equal(5, dt$mon + 1, label="month")
+  expect_equal(21, dt$mday, label="day")
+  expect_equal(0, dt$hour, label="hours")
+  expect_equal(0, dt$min, label="minutes")
+  expect_equal(0, dt$sec, label="seconds")
 })
 
 context("Checks the validity of 4x4")
 
 test_that("is 4x4", {
-  expect_true(isFourByFour("4334-bgaj"), "ok")
-  expect_false(isFourByFour("4334c-bgajc"), "11 characters instead of 9")
-  expect_false(isFourByFour("433-bga"), "7 characters instead of 9")
-  expect_false(isFourByFour("433-bgaj"), "3 characters before dash instead of 4")
-  expect_false(isFourByFour("4334-!gaj"), "non-alphanumeric character")
+  expect_true(isFourByFour("4334-bgaj"), label="ok")
+  expect_false(isFourByFour("4334c-bgajc"), label="11 characters instead of 9")
+  expect_false(isFourByFour("433-bga"), label="7 characters instead of 9")
+  expect_false(isFourByFour("433-bgaj"), label="3 characters before dash instead of 4")
+  expect_false(isFourByFour("4334-!gaj"), label="non-alphanumeric character")
 })
 
 
 test_that("is 4x4 URL", {
-  expect_error(read.socrata("https://soda.demo.socrata.com/api/views/4334c-bgajc"), "11 characters instead of 9")
-  expect_error(read.socrata("https://soda.demo.socrata.com/api/views/433-bga"), "7 characters instead of 9")
-  expect_error(read.socrata("https://soda.demo.socrata.com/api/views/433-bgaj"), "3 characters before dash instead of 4")
-  expect_error(read.socrata("https://soda.demo.socrata.com/api/views/4334-!gaj"), "non-alphanumeric character")
+  expect_error(read.socrata("https://soda.demo.socrata.com/api/views/4334c-bgajc"), "4334c-bgajc is not a valid Socrata dataset unique identifier", label="11 characters instead of 9")
+  expect_error(read.socrata("https://soda.demo.socrata.com/api/views/433-bga"), "433-bga is not a valid Socrata dataset unique identifier", label="7 characters instead of 9")
+  expect_error(read.socrata("https://soda.demo.socrata.com/api/views/433-bgaj"), "433-bgaj is not a valid Socrata dataset unique identifier", label="3 characters before dash instead of 4")
+  expect_error(read.socrata("https://soda.demo.socrata.com/api/views/4334-!gaj"), "4334-!gaj is not a valid Socrata dataset unique identifier", label="non-alphanumeric character")
 })
 
 test_that("Invalid URL", {
-  expect_error(read.socrata("a.fake.url.being.tested"), "invalid url")
+  expect_error(read.socrata("a.fake.url.being.tested"), "a.fake.url.being.tested does not appear to be a valid URL", label="invalid url")
 })
 
 context("Test Socrata with Token")
 
 test_that("CSV with Token", {
   df <- read.socrata('https://soda.demo.socrata.com/resource/4334-bgaj.csv', app_token="ew2rEMuESuzWPqMkyPfOSGJgE")
-  expect_equal(1007, nrow(df), "rows")
-  expect_equal(9, ncol(df), "columns")  
+  expect_equal(1007, nrow(df), label="rows")
+  expect_equal(9, ncol(df), label="columns")  
 })
 
 
 test_that("readSocrataHumanReadableToken", {
   df <- read.socrata('https://soda.demo.socrata.com/dataset/USGS-Earthquake-Reports/4334-bgaj', app_token="ew2rEMuESuzWPqMkyPfOSGJgE")
-  expect_equal(1007, nrow(df), "rows")
-  expect_equal(9, ncol(df), "columns")  
+  expect_equal(1007, nrow(df), label="rows")
+  expect_equal(9, ncol(df), label="columns")  
 })
 
 test_that("API Conflict", {
   df <- read.socrata('https://soda.demo.socrata.com/resource/4334-bgaj.csv?$$app_token=ew2rEMuESuzWPqMkyPfOSGJgE', app_token="ew2rEMuESuzWPqMkyPfOSUSER")
-  expect_equal(1007, nrow(df), "rows")
-  expect_equal(9, ncol(df), "columns")
+  expect_equal(1007, nrow(df), label="rows")
+  expect_equal(9, ncol(df), label="columns")
   # Check that function is calling the API token specified in url
   expect_true(substr(validateUrl('https://soda.demo.socrata.com/resource/4334-bgaj.csv?$$app_token=ew2rEMuESuzWPqMkyPfOSGJgE', app_token="ew2rEMuESuzWPqMkyPfOSUSER"), 70, 94)=="ew2rEMuESuzWPqMkyPfOSGJgE")
 })
 
 test_that("readAPIConflictHumanReadable", {
   df <- read.socrata('https://soda.demo.socrata.com/dataset/USGS-Earthquake-Reports/4334-bgaj?$$app_token=ew2rEMuESuzWPqMkyPfOSGJgE', app_token="ew2rEMuESuzWPqMkyPfOSUSER")
-  expect_equal(1007, nrow(df), "rows")
-  expect_equal(9, ncol(df), "columns")
+  expect_equal(1007, nrow(df), label="rows")
+  expect_equal(9, ncol(df), label="columns")
   # Check that function is calling the API token specified in url
   expect_true(substr(validateUrl('https://soda.demo.socrata.com/dataset/USGS-Earthquake-Reports/4334-bgaj?$$app_token=ew2rEMuESuzWPqMkyPfOSGJgE', app_token="ew2rEMuESuzWPqMkyPfOSUSER"), 70, 94)=="ew2rEMuESuzWPqMkyPfOSGJgE")
 })
@@ -158,8 +158,8 @@ test_that("incorrect API Query", {
   expect_error(read.socrata("https://soda.demo.socrata.com/resource/4334-bgaj.csv?$app_token=ew2rEMuESuzWPqMkyPfOSGJgE"))
   # Check that it was only because of missing $  
   df <- read.socrata("https://soda.demo.socrata.com/resource/4334-bgaj.csv?$$app_token=ew2rEMuESuzWPqMkyPfOSGJgE")
-  expect_equal(1007, nrow(df), "rows")
-  expect_equal(9, ncol(df), "columns") 
+  expect_equal(1007, nrow(df), label="rows")
+  expect_equal(9, ncol(df), label="columns") 
 })
 
 
@@ -168,8 +168,8 @@ test_that("incorrect API Query Human Readable", {
   expect_error(read.socrata("https://soda.demo.socrata.com/dataset/USGS-Earthquake-Reports/4334-bgaj?$app_token=ew2rEMuESuzWPqMkyPfOSGJgE"))
   # Check that it was only because of missing $  
   df <- read.socrata("https://soda.demo.socrata.com/dataset/USGS-Earthquake-Reports/4334-bgaj?$$app_token=ew2rEMuESuzWPqMkyPfOSGJgE")
-  expect_equal(1007, nrow(df), "rows")
-  expect_equal(9, ncol(df), "columns") 
+  expect_equal(1007, nrow(df), label="rows")
+  expect_equal(9, ncol(df), label="columns") 
 })
 
 test_that("List datasets available from a Socrata domain", {
@@ -186,12 +186,6 @@ test_that("List datasets available from a Socrata domain", {
   # Check that all names in data.json are accounted for in ls.socrata return
   expect_equal(as.logical(rep(TRUE, length(names(df)))), names(df) %in% c(core_names))
 })
-
-
-test_that("Invalid Socrata domain", {
-  expect_error(read.socrata("a.fake.url.being.tested"), "invalid url")
-})
-
 
 
 
