971e0511ccf05021e8c5e717edd414e65071d67d
Gene Leynes <gleynes@gmail.com>
2023-08-10 16:35:29 -0500

    initial fix for testthat script

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,12 @@
 library(testthat)
 library(RSocrata)
 
-test_check("RSocrata")
+socrata_status_test_url <- "https://data.cityofchicago.org/resource/xzkq-xp2w.json"
+socrata_status_response <- httr::GET(socrata_status_test_url)
+
+if (httr::status_code(socrata_status_response) == 200) {
+  test_check("RSocrata")
+} else {
+  message("Socrata API is unavailable, skipping tests.")
+}
+
