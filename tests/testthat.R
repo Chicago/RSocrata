a5583a99983fccc3622ddf7f428766561110225a
Gene Leynes <gleynes@gmail.com>
2023-08-24 17:02:45 -0500

    updating comment

--- tests/testthat.R
+++ tests/testthat.R
@@ -20,6 +20,9 @@ Sys.setenv(SOCRATA_PASSWORD="7vFDsGFDUG")
 ## This command will run all files matching test*.R in `./tests`, unless 
 ## the tests are skipped globally or individually. 
 ## Note: Run locally with `devtools::check()`
+## Note: Run this to test as CRAN: Sys.setenv(NOT_CRAN=FALSE)
 
 test_check("RSocrata")
+
+
 
